de95d3a62e3a63a35e6ab98a77e237256ff8f7d1
hugoever <73413517+hugoever@users.noreply.github.com>
2024-07-25 09:31:16 -0400

    First commit

---
+++ src/main/java/py/edu/ucsa/micro/cuentas_api/CuentasApiApplication.java
@@ -0,0 +1,13 @@
+package py.edu.ucsa.micro.cuentas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/ucsa/micro/cuentas_api/CuentasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.ucsa.micro.cuentas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
